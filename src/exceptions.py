edb83afe4ca0ca359ec90588d0e91aef1d853723
SumedhKulkarni97 <kulkarni.sumedh97@gmail.com>
2025-02-16 22:28:54 +0100

    logging and exception3

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,4 @@
 import sys
-import logging
 
 # This function will return the exceptions and where they have occured
 def error_message_detail(error, error_detail:sys):
@@ -17,10 +16,5 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
+
         
